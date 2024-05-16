6c5e497fc1153a30073c1e1d1d457d8c20b25573
Apoorv Vardhan <vardhanapoorv@gmail.com>
2024-05-16 13:32:13 +0530

    Add basic snowflakeID gen using twitter structure

---
+++ snowflake-id-gen/go.mod
@@ -0,0 +1,7 @@
+module snowflakeidgen
+
+go 1.21.6
+
+require github.com/go-sql-driver/mysql v1.8.1
+
+require filippo.io/edwards25519 v1.1.0 // indirect
---
+++ snowflake-id-gen/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func CreateDBConn() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "stnduser:stnduser@tcp(127.0.0.1:3309)/snowflakeidgen")
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func main() {
+
+	db, err := CreateDBConn()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	now := time.Now()
+	// Convert to milliseconds since epoch
+	epochMillis := now.UnixNano() / int64(time.Millisecond)
+	fmt.Println("Epoch Milliseconds:", epochMillis)
+
+	// Check size limits
+	if epochMillis >= (1 << 41) {
+		fmt.Println("Epoch milliseconds too large to fit in 41 bits")
+		return
+	}
+
+	machineID := 1000 // Ensure it is less than 2^10
+	if machineID >= (1 << 10) {
+		fmt.Println("MachineID too large to fit in 10 bits")
+		return
+	}
+
+	counterID := 2000 // Example max for 12 bits
+	if counterID >= (1 << 12) {
+		fmt.Println("CounterID too large to fit in 12 bits")
+		return
+	}
+
+	// Construct the snowflakeID
+	var snowflakeID int64 = (epochMillis << (22)) // Leave space for 10 + 12 bits
+	snowflakeID |= (int64(machineID) << 12)       // Shift left by 12 bits to leave space for the last number
+	snowflakeID |= int64(counterID)               // Place last number in the lowest 12 bits
+
+	// Convert snowflakeID to a binary string
+	binaryStr := strconv.FormatInt(int64(snowflakeID), 2) // Use FormatUint to avoid two's complement
+	// Pad the binary string to ensure it has 64 bits
+	binaryStr = fmt.Sprintf("%064s", binaryStr)         // Pad the string with spaces
+	binaryStr = strings.ReplaceAll(binaryStr, " ", "0") // Replace spaces with zeros
+
+	fmt.Printf("Stored snowflakeID: %d\n", snowflakeID)
+	fmt.Printf("Binary Representation: %s\n", binaryStr)
+}
